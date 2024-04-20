34b1c19a8871fbb6407a6d8677cf46578efea3cf
TimlerTheTerp <165680770+TimlerTheTerp@users.noreply.github.com>
2024-04-20 14:34:42 -0400

    Code

--- main.py
+++ main.py
@@ -3,15 +3,36 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import pandas as pd
 
-driver_path = os.getenv('CHROMEDRIVER_PATH')
-if driver_path:
-    driver = webdriver.Chrome(driver_path)
-else:
-    raise EnvironmentError("Please set the CHROMEDRIVER_PATH environment variable.")
-try:
-    driver.get('https://www.defense.gov/News/Contracts/')
-    #SCRAPE SHIT HEERE
+class WebsiteScraper:
+    def __init__(self, url):
+        self.url = url
+        self.driver = None
 
+    def getDriver(self):
+        driver_path = os.getenv('CHROMEDRIVER_PATH')
+        if not driver_path:
+            raise EnvironmentError("Please set the CHROMEDRIVER_PATH environment variable.")
+        self.driver = webdriver.Chrome(driver_path)
+        return self.driver
+
+    def navigate_to_url(self):
+        """Navigates the driver to the target URL."""
+        self.driver.get(self.url)
+
+
+def main():
+    target_url = "https://www.defense.gov/News/Contracts/"
+
+    scraper = WebsiteScraper(target_url)
+    driver = scraper.getDriver()  # Assuming you want to use the driver elsewhere
+    scraper.navigate_to_url()
+
+    # Your data extraction and processing logic using the driver object goes here
+
+    driver.quit()  # Assuming you want to quit the driver here
+
+if __name__ == "__main__":
+    main()
 
 
 
