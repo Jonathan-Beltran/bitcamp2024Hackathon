171b0c1e289a63f1aff9318c15a9dc5d97b20ad5
Jonathan-Beltran <python8utoo@gmail.com>
2024-04-20 13:40:31 -0400

    sets up selenium

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+import os
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import pandas as pd
 
+driver_path = os.getenv('CHROMEDRIVER_PATH')
+if driver_path:
+    driver = webdriver.Chrome(driver_path)
+else:
+    raise EnvironmentError("Please set the CHROMEDRIVER_PATH environment variable.")
+try:
+    driver.get('https://www.defense.gov/News/Contracts/')
+    #SCRAPE SHIT HEERE
+
+
+
+
+finally:
+    driver.quit()
