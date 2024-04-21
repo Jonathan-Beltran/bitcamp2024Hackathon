34220e193843cd67527b17c858126e41f862e000
TimlerTheTerp <165680770+TimlerTheTerp@users.noreply.github.com>
2024-04-21 09:14:43 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import os
-import spacy
 from selenium import webdriver
 from bs4 import BeautifulSoup
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
 
 class WebsiteScraper:
     def __init__(self, url):
@@ -58,20 +59,29 @@ class WebsiteScraper:
                     self.driver.get(website_url)
                     WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'p')))
                     website_page_source = self.driver.page_source
-                    website_soup = BeautifulSoup(website_page_source, 'html.parser')
+                    website_text = BeautifulSoup(website_page_source, 'html.parser').get_text()
 
-                    # Extract information from the associated website
-                    h1_elements = website_soup.find_all('h1')
-                    for h1 in h1_elements:
-                        print("h1:", h1.get_text())
+                    # Perform text classification
+                    labeled_data = {
+                        'text': [website_text],
+                        'category': ['Federal Agency', 'Values', 'dates', 'company names', 'locations']
+                    }
+                    df = pd.DataFrame(labeled_data)
 
-                    date_elements = website_soup.find_all('span', class_='date')
-                    for date in date_elements:
-                        print("Date:", date.get_text())
+                    vectorizer = TfidfVectorizer()
+                    X = vectorizer.fit_transform(df['text'])
 
-                    p_elements = website_soup.find_all('p')
-                    for p in p_elements:
-                        print("Paragraph:", p.get_text())
+                    classifier = MultinomialNB()
+                    classifier.fit(X, df['category'])
+
+                    predicted_categories = classifier.predict(X)
+
+                    result_df = pd.DataFrame({
+                        'word': df['text'].iloc[0].split(),
+                        'predicted_category': predicted_categories
+                    })
+                    print(result_df)
+
         else:
             print("Main content area not found")
 
@@ -79,63 +89,6 @@ class WebsiteScraper:
         """Closes the web driver."""
         if self.driver:
             self.driver.quit()
-
-def read_file(filename):
-    with open(filename, 'r') as file:
-        lines = file.readlines()
-    return lines
-
-
-def make_table(lines):
-    words = lines.split()
-    categories = ['Federal Agency', 'Values', 'dates', 'company names', 'locations']
-
-    # Flatten the labeled_data dictionary
-    labeled_data = {
-        'text': [],
-        'category': []
-    }
-    for word in words:
-        labeled_data['text'].append(word)
-        labeled_data['category'].append(categories)  # All words are associated with all categories
-
-    # Create DataFrame
-    df = pd.DataFrame(labeled_data)
-
-    categories_array = [category for sublist in df['category'] for category in sublist]
-
-    vectorizer = TfidfVectorizer()
-    x = vectorizer.fit_transform(df['text'])
-
-    classifier = MultinomialNB()
-    classifier.fit(x, categories_array)
-
-    predicted_categories = classifier.predict(X)
-
-    result_df = pd.dataFrame({
-        'word': df['text'],
-        'predicted_category': predicted_categories
-    })
-    print(result_df)
-
-# Jonathans Code
-def process_text_into_ents(filename):
-    nlp = spacy.load("en_core_web_sm")
-    ent_dict = {}
-    with open(filename, 'r') as file:
-        doc = nlp(file.read())
-    for ent in doc.ents:
-        ent_type = ent.label_
-        if ent_type not in ent_dict:
-            ent_dict[ent_type] = [ent.text]
-        else:
-            ent_dict[ent_type].append(ent.text)
-    return ent_dict
-
-
-def process_entsDict_into_lists(ent_dict):
-    ent_lists = {ent_type: ent_texts for ent_type, ent_texts in ent_dict.items()}
-    return ent_lists
 
 def main():
     target_url = "https://www.defense.gov/News/Contracts/"
@@ -147,15 +100,5 @@ def main():
     scraper.extract_information()
     scraper.close_driver()
 
-    filename = 'exampleDOD.txt'
-    ent_dict = process_text_into_ents(filename)
-    ent_lists = process_entsDict_into_lists(ent_dict)
-    print("MY CODE!!!!!!!!")
-    for ent_type, ent_texts in ent_lists.items():
-        print(f"{ent_type}: {ent_texts}")
-
-
-
 if __name__ == "__main__":
     main()
-
